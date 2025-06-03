d8cbbcd58d04302ac9e5129ea6c4dbdaabab728b
TomWilk <tom_wilku@wp.pl>
2025-06-03 16:57:40 +0200

    Initial commit

---
+++ MarkovTrain.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace FantasyNameGenerator
+{
+    internal class MarkovTrain
+    {
+        string path = "namesRaw.txt",
+               trainedFile = "trainedData.json";
+
+        Dictionary<char, Dictionary<char, int>> markov = new Dictionary<char, Dictionary<char, int>>();
+        public MarkovTrain()
+        {
+            if(!File.Exists(path)) 
+            {
+                Console.WriteLine("File does not exist");
+            } 
+            else
+            {
+                var lines = File.ReadLines(path);
+                foreach(var line in lines)
+                {
+                    string name = "^" + line.ToString().Trim().ToLower() + "$";
+                    for(int i = 0, length = name.Length - 1; i < length; i++)
+                    {
+                        char current = name[i];
+                        char next = name[i + 1];
+
+                        if (!markov.ContainsKey(current))
+                        {
+                            markov[current] = new Dictionary<char, int>();
+                        }
+                        if (!markov[current].ContainsKey(next))
+                        {
+                            markov[current][next] = 1;
+                        }
+                        else
+                        {
+                            markov[current][next]++;
+                        }
+                        
+                    }
+                }
+
+                SaveToJson();
+
+            }
+        }
+
+        public void SaveToJson()
+        {
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+
+            string jsonString = JsonSerializer.Serialize(markov, options);
+            File.WriteAllText(trainedFile, jsonString);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace FantasyNameGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                _ = new NameGenerator();
+            } else if (args.Length == 1) 
+            {
+                if (args[0].ToLower() == "--generate")
+                {
+                    _ = new NameGenerator();
+                } else if (args[0].ToLower() == "--train")
+                {
+                    _ = new MarkovTrain();
+                }
+                else
+                {
+                    Console.Error.WriteLine("Error. Błędne użycie");
+                }
+            }
+            else
+            {
+                Console.Error.WriteLine("Error. Błędne użycie");
+            }
+        }
+    }
+}
